b63f9d30d4e31ba1156a4c765511d00ff9431e04
ritesh <riteshchaudhary375@gmail.com>
2025-04-26 12:32:21 +0545

    add middleware and functions to handle errors

--- backend/controllers/todo.controller.js
+++ backend/controllers/todo.controller.js
@@ -1,3 +1,12 @@
-export const testAPI = (req, res) => {
-  res.json({ message: "API is working" });
+import { errorHandler } from "../utils/error.js";
+
+export const testAPI = (req, res, next) => {
+  try {
+    // user build function error
+    // next(errorHandler(400, "All fields required!"));
+
+    res.json({ message: "API is working" });
+  } catch (error) {
+    next(error); // system error
+  }
 };
